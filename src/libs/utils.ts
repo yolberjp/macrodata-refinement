a28f5948f8a3e5281bc223d6361fa8da8180e21a
Yolber Pabon <yolberjp@gmail.com>
2025-03-11 14:46:53 +0100

    Add tailwind-merge dependency and refactor utility function for class merging

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,3 +1,6 @@
-import clsx from "clsx";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-export const cn = clsx;
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
